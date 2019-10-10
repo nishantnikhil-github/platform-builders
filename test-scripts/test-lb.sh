2263ee8071b349e645f37162fac42595d460b129
Olivia.Blaine <olivia.blaine@OliviaBlaines-MacBook-Pro.local>
2019-10-10 14:34:09 +0200

    text scripts for lb

---
+++ test-scripts/test-lb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function kubectl {
+  docker run -v $PWD:/work \
+    -w /work \
+    -v /usr/bin/aws-iam-authenticator:/usr/bin/aws-iam-authenticator \
+    -v /root/.kube/config:/config \
+    --rm --name kubectl \
+    bitnami/kubectl:latest \
+    --kubeconfig /config $@
+}
+
+kubectl get svc | tee lb.txt
+
+val=`cat lb.txt|grep "TYPE"|egrep "LoadBalancer"`
+
+if [ -z "$val" ]
+then
+    echo "Service Not Found"
+    exit 127
+else
+    echo "Service Located"
+    exit 0
+fi
